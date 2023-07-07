4693b497515178acda5180278c357016dfecd001
Joshuva George <joshuva.m@zohocorp.com>
2023-07-07 18:17:23 +0530

    added list for thriukural

--- server.config.ts
+++ server.config.ts
@@ -3,5 +3,5 @@ console.log('process', process.env.NODE_ENV);
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const CONFIG = {
-    apiEndPoint: isDevelopment ? 'http://localhost:3000' : ''
+    apiEndPoint: isDevelopment ? 'http://localhost:3000' : 'http://localhost:3000'
 }
